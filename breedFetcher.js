721304879f4fa0e5f9a0b5f3b4b58912434c0829
Omar Irfan <omar.irfan18@gmail.com>
2021-04-08 22:21:47 +0000

    breed fetcher refactor and test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,39 @@
 const request = require('request');
-const breed = process.argv[2];
+// const breed = process.argv[2];
 
 
-const breedFetcher = function(breedName) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+// const fetchBreedDescription = function(breedName, callback) {
+//   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, _, body) => {
+//     if (error) {
+//       console.log('Error:', error);
+//     }
+//     const data = JSON.parse(body);
+//     if (data.length === 0) {
+//       console.log('Sorry the breed you specified was not found.');
+//       return;
+    
+//     } else {
+//       console.log(data[0].description);
+//     }
+//   });
+
+// };
+
+const fetchBreedDescription = function(breedName, callBack) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, _, body) => {
     if (error) {
-      console.log('Error:', error);
+      return callBack(error, null);
     }
     const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log('Sorry the breed you specified was not found.');
-      return;
-    
+    const breed = data[0];
+    if (breed) {
+      callBack(null, breed.description);
     } else {
-      console.log(data[0].description);
+      callBack("breed doesn't exist", null);
     }
   });
-
 };
 
-breedFetcher(breed);
+module.exports = { fetchBreedDescription };
+
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,30 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+  it('returns "Error fetch details: breed doesnt exist" for invalid breed via callback', (done) => {
+    fetchBreedDescription('omar', (err, desc) => {
+
+      assert.equal(err, "breed doesn't exist");
+
+      const expectedDesc = null;
+
+      assert.equal(expectedDesc, desc);
+
+      done();
+    });
+  });
+});
